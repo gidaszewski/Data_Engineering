85fc018677e415045d271b7399463d443ea973c0
Franco Gidaszewski <francogidaszewski@gmail.com>
2023-06-29 21:43:11 -0300

    Entrega_2

--- script_tabla.sql
+++ script_tabla.sql
@@ -1,5 +1,8 @@
-CREATE TABLE franco_gidaszewski.tipo_de_cambio (
+create table if not exists francogidaszewski_coderhouse.tipo_de_cambio (
     id INT NOT NULL,
-    d_date DATE
-    tipo_de_cambio decimal(10,2)
-); 
+    fecha DATE distkey,
+    tipo_de_cambio decimal(10,2),
+    tasa_politica_monetaria decimal(10,2)
+) sortkey(fecha);
+
+ALTER TABLE ALTER franco_gidaszewski.tipo_de_cambio SORTKEY AUTO;
